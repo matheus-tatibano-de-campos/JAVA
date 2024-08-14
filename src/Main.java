89a0a0b60526f758f428101d9b0fed7f5012f8e1
matheus-tatibano-de-campos <matheustatibano@gmail.com>
2024-08-13 22:23:37 -0300

    Saida de dados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("olá mundo");
+		
+		int y = 32;
+		double x = 10.35784;
+		String nome = "Maria";
+		int idade = 32;
+		double renda = 4000.0;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.println("Bom dia!");
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
+		System.out.println("RESULTADO = " + x + " METROS");
+		System.out.printf("RESULTADO = %.2f METROS%n", x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+		
+		
 
 	}
 
